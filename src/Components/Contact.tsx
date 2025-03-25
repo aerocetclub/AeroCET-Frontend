cc6e3df5aad8a562e9f745951eb986be843fcbbc
S SIDHARTH <sidharthpunalur@gmail.com>
2025-03-25 09:36:18 +0530

    api/contact GET added

--- src/Components/Contact.tsx
+++ src/Components/Contact.tsx
@@ -58,7 +58,7 @@ export default function Contact() {
         <h2>SUPPORT US IN THE UPCOMING SAE <br />Competitions!</h2>
         <button
          onClick={() => {
-          window.location.href = `${import.meta.env.VITE_API_URL}/db/data`;
+          window.location.href = `${import.meta.env.VITE_API_BASE_URL}/api/contact`;
         }}
         >
           <img src={download} alt="Loader-image" className="loader-image h-18 my-4" />
